413561f86230abb1f50dc9e07a4e17389e84a91c
Sagar <gsagarm5@gamil.com>
2022-01-11 01:35:13 +0545

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="splash-container">
+    <div class="splash">
+      <h1 style="font-weight: 100">Fresh Healthy Food</h1>
+    </div>
+  </div>
+
+  <main class="wrapper">
+    <h2>Recommended</h2>
+
+    <div class="recommended">
+
+      <Card
+        v-for="(product, i) in inventory.slice(0, 3)"
+        :key="product.id"
+        class="card"
+        :index="i"
+        :product="product"
+        :addToCart="addToCart"
+      />
+
+    </div>
+  </main>
+</template>
+
+<script>
+import Card from '@/components/Card.vue'
+
+export default {
+  name: 'Home',
+  props: ['inventory', 'addToCart'],
+  components: {
+    Card
+  }
+}
+</script>
---
+++ src/views/Products.vue
@@ -0,0 +1,26 @@
+<template>
+  <main class="wrapper">
+  <h1>Products</h1>
+
+  <div class="card-container">
+    <Card
+      v-for="(product, i) in inventory"
+      :key="product.id"
+      class="card"
+      :index="i"
+      :product="product"
+      :addToCart="addToCart"
+    />
+  </div>
+</main>
+</template>
+<script>
+import Card from '@/components/Card.vue'
+
+export default {
+  props: ['inventory', 'addToCart'],
+  components: {
+    Card
+  }
+}
+</script>
